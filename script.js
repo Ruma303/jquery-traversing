61e5c94b538066a2849ca03daccca2df81925809
Ruma303 <matteoh8@hotmail.it>
2024-01-13 15:12:39 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+
+    //% Metodi traversing
+
+    //# parent()
+    //$("li").parent().parent().css("background-color", "aqua");
+
+    //# parents()
+    //$("li").parents().css("color", "aqua");
+
+    //# parentsUntil()
+    //$("li").parentsUntil("section").css("color", "aqua");
+
+    //# closest()
+    //$("li").closest("div").css("color", "aqua");
+
+    //# children()
+    //$("section").children().css("background-color", "violet");
+
+    //# find()
+    //$("section").find("li:first-child").css("color", "red");
+
+    //# siblings()
+    //$("li").siblings("li").css("color", "red");z
+
+    //# next()
+    //$("li").next("li").css("color", "red");
+
+    //# nextAll()
+    //$("li").nextAll("li").css("color", "red");
+
+    //# nextUntil()
+    //$("li").nextUntil("li:nth-of-type(2n)").css("color", "red");
+
+    //# prev()
+    //$("li:last-of-type").prev().css("color", "red");
+
+    //# prevAll()
+    //$("li:nth-last-of-type(3)").prevAll().css("color", "red");
+
+    //# prevUntil()
+    //$("li:nth-last-of-type(2)").prevUntil("li:nth(2)").css("color", "red");
+
+
+
+    //% Filtering
+
+
+    //# filter()
+
+
+    //# first()
+
+
+    //# last()
+
+
+    //# eq()
+
+
+    //# lt()
+
+
+    //# gt()
+
+
+    //# not()
+
+
+});
